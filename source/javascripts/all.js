717fb4fcc482e349c253782f871d5597578d6ceb
David Zaba <dpzaba@gmail.com>
2015-06-15 10:47:59 +0200

    Added Jquery 2.1.3 in lib (bad practice)

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,8 @@
-//= require_tree ./lib
+//= require ./lib/energize
+//= require ./lib/jquery
+//= require ./lib/jquery_ui
+//= require ./lib/jquery.highlight
+//= require ./lib/jquery.tocify
+//= require ./lib/lunr
+
 //= require_tree ./app
